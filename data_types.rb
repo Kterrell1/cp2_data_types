88c5591cb066149acff8020bedbc71ca8518dbde
Kterrell1 <kterrell1@chicagotechacademy.org>
2014-10-03 12:46:22 -0500

    Update data_types.rb
    Keora Terrell

--- data_types.rb
+++ data_types.rb
@@ -1,11 +1,11 @@
-kaykay = { :age => 21, :height => "5'6\"" }
+Keora = { :age => 18, :height => "5'4\"" }
 
-def say_kaykay
-  "KayKay"
+def say_Keora
+  "Keora"
 end
 
 space = " "
 
-puts say_kaykay + " is" + space + kaykay[:age].to_s + "."
+puts say_Keora + " is" + space + Keora[:age].to_s + "."
 
-puts say_kaykay + " is" + space + kaykay[:height] + "."
+puts say_Keora + " is" + space + Keora[:height] + "."
